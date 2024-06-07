307e917c4a830cd8e4105010efa3b4bfd0a41447
WWiet <104780846+WWiet@users.noreply.github.com>
2024-06-07 14:25:55 -0400

    changing to gruvbox theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,11 +3,13 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "navarasu/onedark.nvim" },
   { "catppuccin/nvim" },
+  { "aktersnurra/no-clown-fiesta.nvim" },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "gruvbox",
     },
   },
 }
